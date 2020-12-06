54e01db233a696f0ed81aa05fb7574708a8caccd
Juan Díaz Sanjurjo <juandisan80@yahoo.es>
2020-12-06 12:18:24 +0100

    Creación servicio rest Item

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/service/ItemServiceImpl.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/service/ItemServiceImpl.java
@@ -1,12 +1,17 @@
 package com.formacionbdi.springboot.app.item.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
 
 @Service
 public class ItemServiceImpl implements IItemService {
@@ -16,14 +21,16 @@ public class ItemServiceImpl implements IItemService {
 
 	@Override
 	public List<Item> findAll() {
-		
-		return null;
+		List<Producto> listaProductos = Arrays.asList(restTemplate.getForObject("http://localhost/listarProductos", Producto[].class));
+		return listaProductos.stream().map(prod -> new Item(prod, 1)).collect(Collectors.toList());
 	}
 
 	@Override
-	public Item findById(Long id, Integer cantidad) {
-		
-		return null;
+	public Item findById(Long idProducto, Integer cantidad) {
+		Map<String, String> pathVariables = new HashMap<>();
+		pathVariables.put("idProducto", idProducto.toString());
+		Producto producto = restTemplate.getForObject("http://localhost/detalleProducto/{idProducto}", Producto.class, pathVariables);
+		return new Item(producto, cantidad);
 	}
 
 }
