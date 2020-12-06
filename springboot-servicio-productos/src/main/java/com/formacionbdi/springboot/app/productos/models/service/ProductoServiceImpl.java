777311d2dfa1e8b31ca645c230d91084fae2ba37
Juan Díaz Sanjurjo <juandisan80@yahoo.es>
2020-12-06 11:16:47 +0100

    Creación repositorio y servicio de entidad Producto

---
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/ProductoServiceImpl.java
@@ -0,0 +1,31 @@
+package com.formacionbdi.springboot.app.productos.models.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.formacionbdi.springboot.app.productos.models.dao.IProductoDao;
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+
+@Service
+public class ProductoServiceImpl implements IProductoService {
+	
+	@Autowired
+	private IProductoDao productoDao;
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Producto> findAll() {
+		
+		return (List<Producto>) productoDao.findAll();
+	}
+
+	@Override
+	public Producto findById(Long idProducto) {
+		
+		return productoDao.findById(idProducto).orElse(null);
+	}
+
+}
