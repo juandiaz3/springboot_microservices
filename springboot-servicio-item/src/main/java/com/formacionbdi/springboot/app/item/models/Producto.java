8dbbea17368456b82c4d29570a83b708f92fe121
Juan Díaz Sanjurjo <juandisan80@yahoo.es>
2020-12-06 11:43:40 +0100

    Creación de microservicio Item

---
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
@@ -0,0 +1,40 @@
+package com.formacionbdi.springboot.app.item.models;
+
+import java.util.Date;
+
+public class Producto {
+	
+	private Long idProducto;
+	private String nombre;
+	private Double precio;
+	private Date createAt;
+	
+	public Producto() {
+	}
+	
+	public Long getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(Long idProducto) {
+		this.idProducto = idProducto;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+}
